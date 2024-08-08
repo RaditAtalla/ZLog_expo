6491e7579b3cc3225fcc6e719f529fd50db3c829
Muhammad Reza Alvi <mrezaalvi@gmail.com>
2024-08-08 20:29:56 +0700

    modified: app/index.js

--- app/index.js
+++ app/index.js
@@ -6,8 +6,17 @@ import colors from "@constants/colors";
 import Input from "@components/Input";
 import Logo from "@components/Logo";
 import { router } from "expo-router";
-
+import { useState } from "react";
 const Login = () => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleLogin(){
+    console.log(email, password);
+    // router.push("./home");
+  }
+
   return (
     <Layout
       statusBarColor={"white"}
@@ -17,13 +26,25 @@ const Login = () => {
         <Logo style={{ alignSelf: "center", marginBottom: 50 }} />
 
         <View style={{ gap: 10 }}>
-          <Input placeholder={"Username"} />
-          <Input placeholder={"Password"} password />
+          <Input 
+            label="Email" 
+            placeholder={"Masukkan email anda"} 
+            value={email}
+            onChangeText={(text) => setEmail(text)}
+          />
+          <Input 
+            label="Password" 
+            placeholder={"Masukkan kata sandi anda"} 
+            password
+            value={password}
+            onChangeText={(text) => setPassword(text)}
+
+          />
         </View>
       </View>
 
       <Button
-        onPress={() => router.push("./home")}
+        onPress={handleLogin}
         color={colors.blue_primary}
         label={"Login"}
       />
