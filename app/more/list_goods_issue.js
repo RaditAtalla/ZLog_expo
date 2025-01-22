0ac3f7343809f304cabfe630113be0f652945c4f
RaditAtalla <radityaderachmadie@gmail.com>
2025-01-22 20:32:21 +0700

    FEAT: create handleDownload function and implement it

--- app/more/list_goods_issue.js
+++ app/more/list_goods_issue.js
@@ -1,57 +1,12 @@
 import React from "react";
 import Layout from "@components/Layout";
-import Logo from "@components/Logo";
-import { Text, View, Platform, Alert } from "react-native";
-import { shareAsync } from "expo-sharing";
-import * as FileSystem from "expo-file-system";
+import { View } from "react-native";
 import Menu from "@components/Menu";
-import colors from "@constants/colors";
 import { router, useLocalSearchParams } from "expo-router";
+import handleDownload from "@lib/utils/handleDownload";
 
 const ListGoodsIssue = () => {
   const { token } = useLocalSearchParams();
-
-  async function handleDownload() {
-    const fileUrl = "http://10.110.0.165:3000/public/goodsIssue.xlsx";
-    const fileName = "goodsIssue.xlsx";
-
-    try {
-      const result = await FileSystem.downloadAsync(
-        fileUrl,
-        FileSystem.documentDirectory + fileName
-      );
-
-      save(result.uri, fileName, result.headers["Content-Type"]);
-    } catch (error) {
-      console.log(error.message);
-    }
-  }
-
-  async function save(uri, fileName, mime) {
-    if (Platform.OS === "android") {
-      const permission =
-        await FileSystem.StorageAccessFramework.requestDirectoryPermissionsAsync();
-      if (permission.granted) {
-        const base64 = await FileSystem.readAsStringAsync(uri, {
-          encoding: FileSystem.EncodingType.Base64,
-        });
-        await FileSystem.StorageAccessFramework.createFileAsync(
-          permission.directoryUri,
-          fileName,
-          mime
-        )
-          .then(async (uri) => {
-            await FileSystem.writeAsStringAsync(uri, base64, {
-              encoding: FileSystem.EncodingType.Base64,
-            });
-          })
-          .catch((e) => console.log(e));
-        Alert.alert("File berhasil didownload");
-      } else {
-        shareAsync(uri);
-      }
-    }
-  }
 
   return (
     <Layout>
@@ -65,7 +20,10 @@ const ListGoodsIssue = () => {
           }
           label={"BPPB"}
         />
-        <Menu label={"Download Goods Issue"} onPress={handleDownload} />
+        <Menu
+          label={"Download Goods Issue"}
+          onPress={handleDownload("goodsIssue.xlsx")}
+        />
       </View>
     </Layout>
   );
--- app/more/list_goods_receipt.js
+++ app/more/list_goods_receipt.js
@@ -1,56 +1,15 @@
 import React from "react";
 import Layout from "@components/Layout";
 import Menu from "@components/Menu";
-import * as FileSystem from "expo-file-system";
-import { shareAsync } from "expo-sharing";
-import { Platform, Alert } from "react-native";
+import handleDownload from "@lib/utils/handleDownload";
 
 const ListGoodsReceipt = () => {
-  async function handleDownload() {
-    const fileUrl = "http://10.110.0.165:3000/public/goodsReceipt.xlsx";
-    const fileName = "goodsReceipt.xlsx";
-
-    try {
-      const result = await FileSystem.downloadAsync(
-        fileUrl,
-        FileSystem.documentDirectory + fileName
-      );
-
-      save(result.uri, fileName, result.headers["Content-Type"]);
-    } catch (error) {
-      console.log(error.message);
-    }
-  }
-
-  async function save(uri, fileName, mime) {
-    if (Platform.OS === "android") {
-      const permission =
-        await FileSystem.StorageAccessFramework.requestDirectoryPermissionsAsync();
-      if (permission.granted) {
-        const base64 = await FileSystem.readAsStringAsync(uri, {
-          encoding: FileSystem.EncodingType.Base64,
-        });
-        await FileSystem.StorageAccessFramework.createFileAsync(
-          permission.directoryUri,
-          fileName,
-          mime
-        )
-          .then(async (uri) => {
-            await FileSystem.writeAsStringAsync(uri, base64, {
-              encoding: FileSystem.EncodingType.Base64,
-            });
-          })
-          .catch((e) => console.log(e));
-        Alert.alert("File berhasil didownload");
-      } else {
-        shareAsync(uri);
-      }
-    }
-  }
-
   return (
     <Layout>
-      <Menu label={"Download Goods Receipt"} onPress={handleDownload} />
+      <Menu
+        label={"Download Goods Receipt"}
+        onPress={handleDownload("goodsReceipt.xlsx")}
+      />
     </Layout>
   );
 };
