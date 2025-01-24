fe223aa390f0f9832b9320b21e7bf10624052c16
RaditAtalla <radityaderachmadie@gmail.com>
2025-01-24 19:24:53 +0700

    FEAT: handle download goods receipt

--- app/more/list_goods_receipt.js
+++ app/more/list_goods_receipt.js
@@ -2,13 +2,23 @@ import React from "react";
 import Layout from "@components/Layout";
 import Menu from "@components/Menu";
 import handleDownloadFromPublic from "@lib/utils/handleDownloadFromPublic";
+import handleDownloadFromAPI from "../../lib/utils/handleDownloadFromAPI";
+import { useLocalSearchParams } from "expo-router";
 
 const ListGoodsReceipt = () => {
+  const { token } = useLocalSearchParams();
+
   return (
     <Layout>
       <Menu
         label={"Download Goods Receipt"}
-        onPress={() => handleDownloadFromPublic("goodsReceipt.xlsx")}
+        onPress={() =>
+          handleDownloadFromAPI(
+            "/goods-receipt/download",
+            "GoodsReceipt.pdf",
+            token
+          )
+        }
       />
     </Layout>
   );
